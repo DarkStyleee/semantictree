14351d392f58486bb38fcec7045189fedd010f59
DarkStyleee <ussaevf@gmail.com>
2022-10-25 18:44:03 +0300

    add graph script

---
+++ src/lib/sigma2.js
@@ -0,0 +1,83 @@
+import Sigma from "sigma";
+import Graph from "graphology";
+
+import ForceSupervisor from "graphology-layout-force/worker";
+
+export function createGraph(container, data) {
+  const graph = new Graph();
+  graph.import(data);
+  graph.nodes().forEach((node, i) => {
+    const angle = (i * 2 * Math.PI) / graph.order;
+    graph.setNodeAttribute(node, "x", 100 * Math.cos(angle));
+    graph.setNodeAttribute(node, "y", 100 * Math.sin(angle));
+  });
+
+  const renderer = new Sigma(graph, container, {
+    renderEdgeLabels: true,
+    allowInvalidContainer: true,
+  });
+
+  const layout = new ForceSupervisor(graph);
+  layout.start();
+
+  const state = { searchQuery: "" };
+
+  function setHoveredNode(node = null) {
+    if (node) {
+      state.hoveredNode = node;
+      state.hoveredNeighbors = new Set(graph.neighbors(node));
+    } else {
+      state.hoveredNode = undefined;
+      state.hoveredNeighbors = undefined;
+    }
+
+    renderer.refresh();
+  }
+
+  renderer.on("enterNode", ({ node }) => {
+    setHoveredNode(node);
+  });
+  renderer.on("leaveNode", () => {
+    setHoveredNode(undefined);
+  });
+
+  renderer.setSetting("nodeReducer", (node, data) => {
+    const res = { ...data };
+
+    if (
+      state.hoveredNeighbors &&
+      !state.hoveredNeighbors.has(node) &&
+      state.hoveredNode !== node
+    ) {
+      res.label = "";
+      res.color = "#f6f6f6";
+    }
+
+    if (state.selectedNode === node) {
+      res.highlighted = true;
+    } else if (state.suggestions && !state.suggestions.has(node)) {
+      res.label = "";
+      res.color = "#f6f6f6";
+    }
+
+    return res;
+  });
+
+  renderer.setSetting("edgeReducer", (edge, data) => {
+    const res = { ...data };
+
+    if (state.hoveredNode && !graph.hasExtremity(edge, state.hoveredNode)) {
+      res.hidden = true;
+    }
+
+    if (
+      state.suggestions &&
+      (!state.suggestions.has(graph.source(edge)) ||
+        !state.suggestions.has(graph.target(edge)))
+    ) {
+      res.hidden = true;
+    }
+
+    return res;
+  });
+}
--- src/views/SemanticTreeView.vue
+++ src/views/SemanticTreeView.vue
@@ -3,22 +3,16 @@
 </template>
 
 <script setup>
-import { createGraph } from "@/lib/sigma";
+import { createGraph } from "@/lib/sigma2";
 import { ref, onMounted } from "vue";
-import graphData from "@/assets/json/golossary.json";
+// import graphData from "@/assets/json/golossary.json";
+import graphData from "@/assets/json/golossary2.json";
 
 const semantic = ref();
-
-// const getData = async (url) => {
-//   const data = await fetch(url);
-//   const result = await data.json();
-//   return result;
-// };
 
 onMounted(() => {
   createGraph(semantic.value, graphData);
   console.log(graphData);
-  // console.log(await getData("/glossary.json"));
 });
 </script>
 
